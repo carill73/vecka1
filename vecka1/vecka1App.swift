ac348bba487caca937ab8c7841fe8ebe3186a6c3
Christer Lindqvist <christer@festinafinance.com>
2023-10-11 18:04:48 +0200

    Initial Commit

---
+++ vecka1/vecka1App.swift
@@ -0,0 +1,17 @@
+//
+//  vecka1App.swift
+//  vecka1
+//
+//  Created by Christer Lindqvist on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct vecka1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
